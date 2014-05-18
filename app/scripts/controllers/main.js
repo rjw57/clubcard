795b5f4528fb14a50eccf33dda73db5be27a1126
Rich Wareham <rjw57@cam.ac.uk>
2014-05-18 14:23:41 +0100

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('clubcardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
